09610f7afd2820da4dd61457984e1eb8dae364d0
Amanpandeya44 <ap5766708@gmail.com>
2025-11-22 13:10:14 +0530

    new code

---
+++ bom.js
@@ -0,0 +1,13 @@
+var a=5
+console.log(a)
+alert ("welcome ankit")
+
+
+function hello(){
+    console.log("hello i am aman")
+}
+function deepak(){
+    console.log("hello i am deepak")
+}
+hello()
+deepak()
---
+++ index.js
@@ -0,0 +1,4 @@
+var israining = true;
+var areyouawesome = false;
+console.log(israining);
+console.log(areyouawesome);
