7f35c327930e8bc4175f602d4d1635f528e99646
Amanpandeya44 <ap5766708@gmail.com>
2025-11-24 16:25:43 +0530

    code update

--- bom.js
+++ bom.js
@@ -1,17 +1,56 @@
-console.log(a)
+// console.log(a)
 // alert ("welcome ankit")
 
 
-function hello(){
-    console.log("hello i am aman")
-}
-function deepak(){
-    console.log("hello i am deepak")
-}
-hello()
-deepak()
+// function hello(){
+//     console.log("hello i am aman")
+// }
+// function deepak(){
+//     console.log("hello i am deepak")
+// }
+// hello()
+// deepak()
 // console.log(a==b)
-var a=5
-var b='5'
+// var a=5
+// var b='5'
 // === is used for checking data type and comparison
-console.log(a===b)
+// console.log(a===b)
+
+// 1st priority string
+// 2nd priority number
+ 
+// function add(){
+//     var a=5
+//     var b=5
+//     var c="d"
+//     console.log(a+b+c)
+// }
+// add()
+
+
+// function greet(){
+//     // bom=> browser object model(alert(),prompt())
+//    var userans =  window.prompt("hello")
+//    console.log(userans)
+//    if (userans>=18){
+//     window.alert("u can vote")
+//    }
+//    else{
+//     window.alert("u cant vote")
+//    }
+// }
+// greet()
+
+
+
+
+function greet(){
+    var userans=prompt("enter a number")
+    if (userans <0){
+        window.alert(-userans)
+    }
+    else{
+        window.alert(userans)
+    }
+}
+greet()
