ba709042927c968f3df5670c13887698c4121801
Amanpandeya44 <ap5766708@gmail.com>
2025-12-01 15:41:14 +0530

    some new code

---
+++ object.js
@@ -0,0 +1,27 @@
+// we can perform CRUD opertaion
+// let num = 1
+// let str = "aman"
+// let arr = ["aman",21,"bhopal"]
+
+// key : value
+let object ={
+    myname:"aman",
+    myage : 21,
+    mycity : "bhopal",
+    fruits : ["apple","grapes","lemon"]
+}
+
+// console.log(object.myage) 
+// console.log(object).       access object value
+// object.myfees = 50000      assign new value update
+// delete object.myname       delete 
+// console.log(object.fruits[1])
+
+function hello(){
+let myname = "aman"
+
+    alert(myname)
+}
+function hii(){
+    alert(myname = "deepak")
+}
