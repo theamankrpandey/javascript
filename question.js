c3fe32018ab2ce9aa2b68b5025366f454ea39bf0
Amanpandeya44 <ap5766708@gmail.com>
2025-12-07 23:19:52 +0530

    js Q&A

---
+++ question.js
@@ -0,0 +1,29 @@
+// 1) Create an array of 5 numbers and use a for loop to print each number
+// using an arrow function inside the loop.
+
+// let arr = [10, 20, 30, 40, 50];
+// for (let i=0; i<arr.length; i++){
+//     let a =(n)=> console.log(n)
+//     a(arr[i])
+// }
+
+// 2) Using an array of names, check inside a for loop:
+// If the name length is greater than 5, print "Long Name",
+// else print "Short Name".
+
+
+// let arr = ["Alice", "Jonathan", "Bob", "Christine"];
+// for (let i=0; i<arr.length; i++){
+//     if (arr[i].length>5){
+//         console.log("long name");
+//     }
+//     else{
+//         console.log("short name")
+//     }
+// }
+
+
+
+// 3) Create an object student with properties: name, age, city.
+// Use a for…in loop to print all keys and values.
+
