4d17817743e366279788cfd8257e098a287a6526
Amanpandeya44 <ap5766708@gmail.com>
2025-12-11 20:16:24 +0530

    login and signup page

--- localstorage.js
+++ localstorage.js
@@ -1,16 +1,24 @@
 let setdata=()=>{
-    let data{
-        name="aman",
-        age=25,
-        location="bhopal"
-    }
-    localStorage.setItem("myname","aman")
-    localStorage.setItem("myage",21)
+    // let mydata={
+    //     name="aman",
+    //     age=25,
+    //     location="bhopal"
+    // }
+
+    let name ="sid"
+
+    localStorage.setItem("myname",name)
+    // localStorage.setItem("myname","aman")
+    // localStorage.setItem("myage",21)
     location.reload()
 }
 
 let show=document.querySelector('#okk')
+let data = 
+
 show.innerHTML=localStorage.getItem("myname")
+
+let 
 
 let removedata=()=>{
     localStorage.removeItem("myname")
--- login_signup_page/local.js
+++ login_signup_page/local.js
@@ -1,8 +1,8 @@
-let formvalidate=()=>{
+let signup=()=>{
     let name=document.querySelector('#name').value.trim()
     let number=document.querySelector('#number').value.trim()
     let email=document.querySelector('#email').value.trim()
-    let pass=document.querySelector('#pass').value.trim()
+    let password=document.querySelector('#pass').value.trim()
     let Cpass=document.querySelector('#cpass').value.trim()
 
     // p tag
@@ -46,19 +46,39 @@ let formvalidate=()=>{
         return false
     }
 
-    if(pass==""){
+    if(password==""){
         errpass.innerHTML="please enter your password !"
         errpass.style.color="red"
         return false
     }
+    else if(password.match(/[1234567890]/ && password.match(/[!@#$%^&*]/)
+             && password.match(/[A-Z]/) && password.match(/[a-z]/)))
+            errpass.innerHTML="please enter a strong password"
     
-    else if(Cpassword!=Password){
+    else if(Cpass!=password){
         alert("password does not match")
         return false
     }
 
-    else if(Password.match(/[1234567890]/ && Password.match(/[!@#$%^&*]/)
-             && Password.match(/[A-Z]/) && Password.match(/[a-z]/)))
-            errpass.innerHTML="please enter a strong password"
 
+localStorage.setItem("name",name)
+localStorage.setItem("number",number)
+localStorage.setItem("email",email)
+localStorage.setItem("password",password)
 }
+let login = () => {
+    let name = document.querySelector('#loginname').value.trim();
+    let password = document.querySelector('#loginpass').value.trim();
+
+    let storename = localStorage.getItem("name");
+    let storepass = localStorage.getItem("password");
+
+    if (name === storename && password === storepass) {
+        window.location.href = "home.html";
+        return false;
+    } else {
+        alert("Invalid Credential");
+        return false;
+    }
+}
+
