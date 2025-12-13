42451e5952014b1aefe4af098a864568d3161578
Amanpandeya44 <ap5766708@gmail.com>
2025-12-13 22:17:15 +0530

    map

---
+++ map.js
@@ -0,0 +1,51 @@
+// map
+// let arr=[2,3,4,5,6,7,8]
+// // for(initalize; condition; increment/decrement)
+
+// let newarr=arr.map((e)=>{
+//     return e*2
+// })
+// console.log(newarr);
+
+
+
+// filter 
+
+// let arr=[2,3,4,5,6,7,8]
+
+// let newarr=arr.filter((e)=>{
+//     return e%2==0
+// })
+// console.log(newarr);
+
+
+
+// reduce
+// let arr=[2,3,4,5,6,7,8]
+
+// let newarr=arr.reduce((e,n)=>{
+//     return (e+n)
+// },0)
+// console.log(newarr);
+
+
+
+let data=[{
+    name:"aman",
+    age:21,
+    city:"bhopal"
+},{
+   name:"deepak",
+    age:21,
+    city:"kolkata" 
+},{
+    name:"shravan",
+    age:21,
+    city:"bihar"
+}]
+
+let new=data.map((e)=>{
+    return e
+})
+
+console.log(new)
